9ca4acf25341880f5bd776791402a8cf90cab163
Herbert540 <93451142+Herbert540@users.noreply.github.com>
2022-03-12 01:17:11 -0600

    Initial commit

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.kt
@@ -1,8 +1,12 @@
 package com.codepath.apps.restclienttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -51,6 +55,40 @@ class TimelineActivity : AppCompatActivity() {
         populateHomeTimeline()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    // Handles clicks on menu item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.compose) {
+            // Navigate to compose screen
+            val intent = Intent(this, ComposeActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+
+            //Get data from our intent
+            val tweet = data?.getParcelableExtra<Tweet>("tweet") as Tweet
+
+            // Update timeline
+            tweets.add(0,tweet)
+
+            // Update adapter
+            adapter.notifyItemInserted(0)
+            rvTweets.smoothScrollToPosition(0)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+
 
     fun populateHomeTimeline() {
         client.getHomeTimeline(object: JsonHttpResponseHandler(){
@@ -76,10 +114,12 @@ class TimelineActivity : AppCompatActivity() {
             override fun onFailure(statusCode: Int, headers: Headers?, response: String?,
                                    throwable: Throwable?) {
                 Log.i(TAG,"onFailure $statusCode")
+
             }
         })
     }
     companion object {
         val TAG = "TimelineActivity"
+        val REQUEST_CODE = 10
     }
 }
